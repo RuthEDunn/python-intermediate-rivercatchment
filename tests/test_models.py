610f02e7f7456ef3f7eecfacd103e03eb6b87891
Ruth Dunn <ruthiedunn@hotmail.co.uk>
2024-03-19 10:36:16 +0000

    Adding Tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,3 +49,63 @@ def test_daily_mean_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+    
+    """Tests for statistics functions within the Model layer."""
+
+import pandas as pd
+import pandas.testing as pdt
+import datetime
+from catchment.models import daily_mean
+import pytest
+
+def test_daily_mean_zeros():
+    """Test that mean function works for an array of zeros."""
+    from catchment.models import daily_mean
+
+    test_input = pd.DataFrame(
+                     data=[[0.0, 0.0],
+                           [0.0, 0.0],
+                           [0.0, 0.0]],
+                     index=[pd.to_datetime('2000-01-01 01:00'),
+                            pd.to_datetime('2000-01-01 02:00'),
+                            pd.to_datetime('2000-01-01 03:00')],
+                     columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[0.0, 0.0]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
+
+    # Need to use Pandas testing functions to compare arrays
+    pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_mean_integers():
+    """Test that mean function works for an array of positive integers."""
+    from catchment.models import daily_mean
+
+    test_input = pd.DataFrame(
+                     data=[[1.0, 2.0],
+                           [3.0, 4.0],
+                           [5.0, 6.0]],
+                     index=[pd.to_datetime('2000-01-01 01:00'),
+                            pd.to_datetime('2000-01-01 02:00'),
+                            pd.to_datetime('2000-01-01 03:00')],
+                     columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[3.0,4.0]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
+
+    # Need to use Pandas testing functions to compare arrays
+    pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+def test_daily_min_python_list():
+    """Test for AttributionError when passing a python list"""
+    from catchment.models import daily_min
+
+    with pytest.raises(AttributeError):
+        error_expected = daily_min([3, 4, 7], [-3, 0, 5])
